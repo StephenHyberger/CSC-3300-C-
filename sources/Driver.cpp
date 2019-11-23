932733d17788846f1758fa7fe33245d97a1d7bc5
NoahWilmore <nfwilmore42@students.tntech.edu>
2019-11-23 14:44:47 -0600

    Push

--- sources/Driver.cpp
+++ sources/Driver.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
-#include<mysqlx\xdevapi.h>
-//using namespace std;
+
+using namespace std;
 
 int main() {
-	std::cout << ("Hello World\n");
-	std::cout << ("Hello Again\n");
-	std::cout << ("Let's get this bread.");
+	cout << ("Hello World\n");
+	cout << ("Hello Again\n");
+	cout << ("Let's get this bread.");
 	//Ends program.
 	return 0;
 }

87bf1f10a86d17706ffec0c8d570b22240683e34
NoahWilmore <nfwilmore42@students.tntech.edu>
2019-11-24 14:40:22 -0600

    Updated Probelms 1 and 2

--- Problem2.py
+++ Problem2.py
@@ -1,7 +1,7 @@
 #Author: Noah Wilmoree
-#File: Problem1.py
+#File: Problem2.py
 #Date: 11.24.2019
-#Purpose: Demonstrates selection, grouping, and average of a table, instructor, by a strong, medium, and weak user through code imbeded in python.
+#Purpose: Demonstrates selection and update of a table, instructor, by a strong and medium user through code imbeded in python.
 
 import mysql.connector
 
@@ -15,18 +15,19 @@ med_connect = mysql.connector.connect(user='medium',
 							host='localhost',
 							database='assignment4')
 
-weak_connect = mysql.connector.connect(user='weak', 
-							password='password1',
-							host='localhost',
-							database='assignment4')
-
 strong_cursor = strong_connect.cursor()
 med_cursor = med_connect.cursor()
-weak_cursor = weak_connect.cursor()
 
-select = ('SELECT dept_name, AVG(salary) FROM instructor GROUP BY dept_name');
+select = ('SELECT dept_name, salary FROM instructor');
+update = ('UPDATE instructor SET salary = salary * 1.2 WHERE salary < 65000');
 
-print('\nStrong select:')
+print('\nTable before:')
+strong_cursor.execute(select)
+for (dept_name, salary) in strong_cursor:
+    print('{} {}'.format(dept_name, salary))
+
+print('\nTable after strong update:')
+strong_cursor.execute(update)
 strong_cursor.execute(select)
 for (dept_name, salary) in strong_cursor:
 	print('{} {}'.format(dept_name, salary))
@@ -34,18 +35,11 @@ for (dept_name, salary) in strong_cursor:
 strong_cursor.close()
 strong_connect.close()
 
-print('\nMedium select:')
+print('\nTable after medium update:')
+med_cursor.execute(update)
 med_cursor.execute(select)
 for (dept_name, salary) in med_cursor:
 	print('{} {}'.format(dept_name, salary))
 
 med_cursor.close()
 med_connect.close()
-
-print('\nWeak select:')
-weak_cursor.execute(select)
-for (dept_name, salary) in weak_cursor:
-	print('{} {}'.format(dept_name, salary))
-
-weak_cursor.close()
-weak_connect.close()

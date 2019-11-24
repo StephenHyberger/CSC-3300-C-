97e20d82d31d1bc5f1d6dac5d1bf0cebd5c021aa
StephenHyberger <54510340+StephenHyberger@users.noreply.github.com>
2019-11-24 13:44:53 -0600

    Finished 4.5

---
+++ asg4p5.py
@@ -0,0 +1,34 @@
+#Author: Stephen Hyberger
+#File: asg4p5.py
+#Date: 11.24.2019
+#Purpose: Demonstrates removal of a table, course, by a strong user through code imbeded in python.
+
+import mysql.connector
+
+cnx = mysql.connector.connect(user='strong',
+							password='password3',
+							host='localhost',
+							database='assignment4')
+
+cursor = cnx.cursor()
+query = ('SHOW TABLES')
+cursor.execute(query)
+
+print('\nTables before:')
+for(item) in cursor:
+	print(item)
+
+query = ('DROP TABLE IF EXISTS course')
+cursor.execute(query)
+
+print('\nRemoved course table from database')
+
+query = ('SHOW TABLES')
+cursor.execute(query)
+
+print('\nTables after:')
+for(item) in cursor:
+	print(item)
+
+cursor.close()
+cnx.close()
